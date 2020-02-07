cd197d43c05e2768dcd181c03c7ed6b218e7a213
sagarutlas-zapbuild <sagarutlas.zapbuild@gmail.com>
2020-02-07 16:45:16 +0530

    Day2 commit

--- project1/urls.py
+++ project1/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('register/', include('register.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-     path('', TemplateView.as_view(template_name='home.html'), name='home')
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
--- register/urls.py
+++ register/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include # new
-from django.views.generic.base import TemplateView
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), # new
+    # path('aman/',views.aman)
 ]
