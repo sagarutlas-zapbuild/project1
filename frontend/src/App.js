751aa2d581f9ad2c7645452abe34fa4167e6ba85
Aman Saxena <you@example.com>
2020-02-14 10:36:14 +0530

    almost ready lead form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LeadDashboard from './Components/LeadDashboard'
+//import LeadDashboard from './Components/LeadDashboard'
+import Leadform from './Components/Leadform';
 
 function App() {
   return (
-    <LeadDashboard/ >
+    <Leadform/ >
   );
 }
 
