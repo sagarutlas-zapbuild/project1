b4d2565e88be3fa7ed513a9982c8fe6003d0cd35
sagarutlas-zapbuild <sagarutlas.zapbuild@gmail.com>
2020-02-18 10:15:52 +0530

    Tried to fix the issue with app.js. It showed error everytime we pulled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 //import LeadDashboard from './Components/LeadDashboard'
-import Leadform from './Components/Leadform';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import LeadDashboard from './Components/LeadDashboard'
 import Leadform from './Components/Leadform';
+import Dashboard from './Components/LeadDashboard'
 
 function App() {
   return (
