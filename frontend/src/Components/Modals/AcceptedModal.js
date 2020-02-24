b4d7b8f8b03a7c78b06c80b9ba329da56b6a4f16
Aman Saxena <you@example.com>
2020-02-24 15:19:53 +0530

    new modals changes

--- frontend/src/Components/Modals/AcceptedModal.js
+++ frontend/src/Components/Modals/AcceptedModal.js
@@ -12,14 +12,55 @@ const AcceptedModal = (props) => {
 
   return (
     <div>
-      
+
       <Label className="Radio-label">{labelText}</Label>
       <input type="radio" name="new" onClick={toggle}></input>
-    
+
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
         <ModalBody>
-        Accepted Modal <br/> {labelText}        </ModalBody>
+
+          <div class="container">
+            <div class="row">
+
+              <div class="col-sm-6">
+
+                <input type="text">
+
+                </input>
+                <label className="text-center">Prospect Detail</label>
+                <textarea
+                  id="description_new"
+                  rows="3" required
+                />
+              </div>
+
+
+
+              <div class="col-sm-6">
+                <div id="margin"   padding="0px">
+                  <div className="sidenav">
+                    <div className="right">
+                      <div className="form-group">
+                        <label className="text-center">Prospect Detail</label>
+                        <br />
+                        <a href="#">Services</a>
+                        <br />
+                        <a href="#">Clients</a>
+                        <br />
+                        <a href="#">Contact</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+
+            </div>
+          </div>
+
+        </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
           <Button color="secondary" onClick={toggle}>Cancel</Button>
