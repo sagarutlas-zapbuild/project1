c9e6570f05503cc9c05ae705068f5ff420f9643e
Aman Saxena <you@example.com>
2020-02-25 10:13:50 +0530

    modal size is fixed and now working with design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 //import LeadDashboard from './Components/LeadDashboard'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import LeadDashboard from './Components/LeadDashboard'
-import Leadform from './Components/Leadform';
+//import Leadform from './Components/Leadform';
 import Dashboard from './Components/LeadDashboard'
 
 function App() {
--- frontend/src/Components/Modals/NewModal.js
+++ frontend/src/Components/Modals/NewModal.js
@@ -21,70 +21,81 @@ const [modal, setModal] = useState(false);
   }
 
   return (
-    <div>
-
-<Label className="Radio-label">{labelText}</Label>
+ 
+<div>
+  <Label className="Radio-label">{labelText}</Label>
 <input type="radio" name="new" onClick={toggle}></input>
-
-<Modal isOpen={modal} toggle={toggle}>
+  <Modal className="modal-dailog modal-lg" isOpen={modal} toggle={toggle}>
 <ModalHeader toggle={toggle}>{labelText}</ModalHeader>
 <ModalBody>
-{/* <Container fluid={true} className="Full-screen">
-*/}
-<form>
-<div id="margin">
-{/* <div id="form-control">
-*/} <row>
-<Col>
-<div className="sidenav">
-<div className="right">
-<div className="form-group">
-<a href="#">About</a>
-<br />
-<a href="#">Services</a>
-<br />
-<a href="#">Clients</a>
-<br />
-<a href="#">Contact</a>
-</div>
-</div>
-</div>
-</Col>
-<Col>
-<textarea
-className="form-control"
-id="description_new"
-rows="3 " required
-/>
-<br/>
+<div class="container">
+            <div class="row">
 
-<div className="form-group">
-<Col>
-<label className="text-center">Prospect Detail</label>
-</Col>
-<textarea
-className="form-control"
-id="description_new"
-rows="3 " required
-/>
+              <div class="col-sm-8">
 
-</div>
-</Col>
+                <textarea
+                  id="description_new"
+                  rows="3" cols="30" required
+                 /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
 
-</row>
+ <textarea value={value}  defaultValue="val" /> */
+                />
+<div id="margin1">
+                <label className="text-center">Prospect Detail</label>
 
-</div>
-{/* </div>
-*/} </form>
-{/* </Container>
-*/} </ModalBody>
+                </div>
+                <div id="margin1" wrap-content>
+                  <labelText id="margin1" > </labelText>
+                </div>
+                <label className="text-center">Attachement</label>
+
+              </div>
+
+
+
+              <div class="col-sm-4">
+                <div id="margin1">
+                  <div className="sidenav">
+                      
+                        <label ><font size="2">  Prospect Detail</font> </label>
+                        <br />
+                        <label >TAGS</label>
+                        <br/>
+
+                        <a href="#">Services</a>
+                        <br />
+                        <a href="#">Clients</a>
+                        <br />
+                        <a href="#">Contact</a>
+                        <br/>
+
+                        <a href="#">Services</a>
+                        <br />
+                        <a href="#">Clients</a>
+                        <br />
+                        <a href="#">Contact</a>
+                                                <br/>
+
+                        <a href="#">Services</a>
+                        <br />
+                        <a href="#">Clients</a>
+                        <br />
+                        <a href="#">Contact</a>
+
+                        
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+</ModalBody>
 <ModalFooter>
 <Button color="primary" onClick={toggle}>Accept</Button>{' '}
 <Button color="primary" onClick={toggleNested}>Reject</Button>
 {/*props still need to be made.
 Hence, complete functionality is still not achieved for second level modal*/}
 <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
-            <ModalHeader>Nested Modal title</ModalHeader>
+            <ModalHeader>Reason For rejection</ModalHeader>
             <ModalBody>
             <form>
 <div id="margin">
@@ -92,7 +103,7 @@ Hence, complete functionality is still not achieved for second level modal*/}
 <textarea
 className="form-control"
 id="description_new"
-rows="3" required
+rows="5" required
 />
 <Button color="primary" onClick={toggle}>Submit</Button>
 </div>
@@ -105,7 +116,7 @@ rows="3" required
           </Modal>
 </ModalFooter>
 </Modal>
-    </div>
+</div>
   );
 }
 export default NewModal;
