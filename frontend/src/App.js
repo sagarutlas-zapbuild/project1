afc0aebc8c164198f0c0ac32ab1e391de3500fb1
sagarutlas-zapbuild <sagarutlas.zapbuild@gmail.com>
2020-02-27 12:56:09 +0530

    API get seccessful in Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 //import LeadDashboard from './Components/LeadDashboard'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,10 +7,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Leadform from './Components/Leadform';
 import Dashboard from './Components/LeadDashboard'
 
-function App() {
-  return (<>
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      prospect: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/prospects/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            prospect: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render(){
+    return (<>
     <Leadform/>
-    </>);
+    {this.state.prospect.map((prospect, index) => {
+      return(
+        <li>
+          {prospect.prospect_id}
+          {prospect.prospect_full_name}
+          {prospect.prospect_company}
+        </li>
+      )
+    })}
+
+    </>);}
      
 }
 export default App;
--- project1/settings.py
+++ project1/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'phone_field',
-    'API'
-    
+    'API',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://192.168.1.175:3000',
+)
