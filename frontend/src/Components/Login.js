c6f2cf972601cb0c61c7b1c096de3f5ced44b54c
amanzap2610 <amansaxena.zapbuild@gmail.com>
2020-03-04 16:44:19 +0530

    login validator and modals have been restored

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,30 +1,101 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
- 
-function Login() {
-    return (
-        <div id="margin">
-            <label>Login
-                </label>
-            <br />
-            <br />
-            <label>
-                <b>Username : </b>
-                <input type="text" name="Username" required placeholder="username">
-                </input>
-            </label>
-            <br />
-            <br />
-            <label>
-                <b>Password : </b>
-                <input type="password" name="password"  required placeholder="password">
-                </input>
-            </label>
-            <br />
-            <br />
-           <button  type="submit" onClick={() => window.location.reload(true)}><Link to="leaddash"></Link>submit</button>
-        </div>
+import React from "react";
+import { Formik } from "formik";
+//import * as usernameValidator from "username-validator";
+import * as Yup from "yup";
+import {Link} from "react-router-dom";
+const Login = () => (
+  <Formik
+    initialValues={{ username: "", password: "" }}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        console.log("Logging in", values);
+        setSubmitting(false);
+      }, 500);
+    }}
+    //********Handling validation messages yourself*******/
+    // validate={values => {
+    //   let errors = {};
+    //   if (!values.username) {
+    //     errors.username = "Required";
+    //   } else if (!usernameValidator.validate(values.username)) {
+    //     errors.username = "Invalid username address";
+    //   }
 
-    );
-}
+    //   const passwordRegex = /(?=.*[0-9])/;
+    //   if (!values.password) {
+    //     errors.password = "Required";
+    //   } else if (values.password.length < 8) {
+    //     errors.password = "Password must be 8 characters long.";
+    //   } else if (!passwordRegex.test(values.password)) {
+    //     errors.password = "Invalida password. Must contain one number";
+    //   }
+
+    //   return errors;
+    // }}
+    //********Using Yum for validation********/
+
+    validationSchema={Yup.object().shape({
+      username: Yup.string()
+        .required("Required"),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum.")
+        .matches(/(?=.*[0-9])/, "Password must contain a number.")
+    })}
+  >
+    {props => {
+      const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        
+      } = props;
+      // validateButton(){
+      //   if (username && password == nul)
+      // }
+      return (
+        <div className="middle">
+
+        <form >
+          <label htmlFor="username"><h4><b>Username:</b></h4></label>
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your Username"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.username && touched.username && "error"}
+          />
+          {errors.username && touched.username && (
+            <div className="input-feedback">{errors.username}</div>
+          )}
+          <br/>
+          <br/>
+          <label htmlFor="username"><h4><b>Password:</b></h4></label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.password && touched.password && "error"}
+          />
+          {errors.password && touched.password && (
+            <div className="input-feedback">{errors.password}</div>
+          )}
+          <br/>
+          <br/>
+          <Link to="/leaddash"><button class="btn btn-success" type="submit">submit</button>  </Link>
+        
+        </form>
+        </div>
+      );
+    }}
+  </Formik>
+);
+
 export default Login;
---
+++ frontend/src/Components/Modals/NewModal.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label } from 'reactstrap';
+//import {AcceptedModal} from './AcceptedModal'
+
+
+const NewModal = (props) => {
+  const {
+    labelText
+  } = props;
+
+  const [modal, setModal] = useState(false);
+  const [nestedModal, setNestedModal] = useState(false);
+  const [closeAll, setCloseAll] = useState(false);
+
+  const toggle = () => setModal(!modal);
+  const toggleNested = () => {
+    setNestedModal(!nestedModal);
+    setCloseAll(false);
+  }
+
+  return (
+
+    <div>
+      <Label className="Radio-label">{labelText}</Label>
+      <input type="radio" name="new" onClick={toggle}></input>
+      <Modal className="modal-dailog modal-lg" isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>{labelText}</ModalHeader>
+        <ModalBody>
+          <div class="container">
+            <div class="row">
+
+              <div class="col-sm-8">
+
+                <textarea
+                  className="top"
+                  id="description_new"
+                  rows="5" cols="51" required
+                /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
+<textarea value={value}  defaultValue="val" /> */
+                />
+                <div id="margin1" class="float-left w3-border w3-padding">
+                  <label className="text-center"><b>Prospect Detail</b></label>
+
+                </div>
+                <textarea
+                  className="top"
+                  id="description_new"
+                  rows="5" cols="51" required
+                /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
+<textarea value={value}  defaultValue="val" /> */
+
+                />
+
+                <label className="text-center top"><b>Attachement</b></label>
+
+              </div>
+              <div class="col-sm-4">
+                <div id="margin1">
+                  <div className="sidenav">
+                    <div id="margin1">
+                      <b>
+                        <label style={{ textAlign: "center" }}><font size="3" > NEW</font> </label>
+                      </b>
+                    </div>
+
+                    <br />
+                    <label >TAGS</label>
+                    <br />
+
+                    <textarea
+                      id="description_new"
+                      rows="3" cols="20" required
+                    /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
+   
+    <textarea value={value}  defaultValue="val" /> */
+                    />
+                    <label >DOMAIN</label>
+                    <br />
+
+                    <textarea
+                      id="description_new"
+                      rows="3" cols="20" required
+                    /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
+   
+    <textarea value={value}  defaultValue="val" /> */
+                    />
+                    <label >TECHNOLOGY</label>
+                    <br />
+
+                    <textarea
+                      id="description_new"
+                      rows="3" cols="20" required
+                    /*  let value = this.state.data.map(e=>JSON.stringify(e).replace(/{|}/g,'')).join(',\n');
+   
+    <textarea value={value}  defaultValue="val" /> */
+                    />
+
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Accept</Button>{' '}
+          <Button color="primary" onClick={toggleNested}>Reject</Button>
+          {/*props still need to be made.
+Hence, complete functionality is still not achieved for second level modal*/}
+          <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
+            <ModalHeader>Reason For rejection</ModalHeader>
+            <ModalBody>
+              <form>
+                <div id="margin">
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      id="description_new"
+                      rows="5" required
+                    />
+
+                  </div>
+                </div>
+              </form>
+            </ModalBody>
+            <ModalFooter>
+
+              <Button color="primary" onClick = {toggle}>Submit</Button>
+            </ModalFooter>
+          </Modal>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+export default NewModal; 
